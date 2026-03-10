fdacd5314b3236c0ddc87a90fe33db7954565fb0
Muyizereberissa <muyizereberissa22@gmail.com>
2026-03-11 01:53:35 +0200

    improved the signup screen UI

--- lib/screens/auth/signup_screen.dart
+++ lib/screens/auth/signup_screen.dart
@@ -116,6 +116,10 @@ class _SignupScreenState extends State<SignupScreen> {
 
                   setState(() => isLoading = true);
 
+                  await auth.signUp(
+                    emailController.text.trim(),
+                    passwordController.text.trim(),
+                  );
 
                   setState(() => isLoading = false);
 
