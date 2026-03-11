113f6e0b522fb7b0838a15c4036062941f91e293
Muyizereberissa <muyizereberissa22@gmail.com>
2026-03-11 02:01:41 +0200

    updated place provider

--- lib/providers/place_provider.dart
+++ lib/providers/place_provider.dart
@@ -5,11 +5,6 @@ import '../services/firestore_service.dart';
 class PlaceProvider extends ChangeNotifier {
   final FirestoreService _service = FirestoreService();
 
-  List<Place> _places = [];
-  bool _isLoading = false; // <-- Add loading state
-
-  List<Place> get places => _places;
-  bool get isLoading => _isLoading; // <-- Getter for loading
 
   PlaceProvider() {
     listenToPlaces();
@@ -17,13 +12,13 @@ class PlaceProvider extends ChangeNotifier {
 
   void listenToPlaces() {
     print("Listening to Firestore...");
-    _isLoading = true; // start loading
+  
     notifyListeners();
 
     _service.getPlaces().listen((data) {
       print("Loaded places: ${data.length}");
       _places = data;
-      _isLoading = false; // finished loading
+      _isLoading = false; 
       notifyListeners();
     });
   }
